175d6d122aec95dc832d770a6b883bb8fe95792f
kikyungchan <kikyungchan@gmail.com>
2025-07-09 10:43:50 +0900

    제목, 본문, 작성자 값이 비었을때 400응답과 toast띄우기

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import { MainLayout } from "./MainLayout.jsx";
+import { MainLayout } from "./common/MainLayout.jsx";
 import { BoardAdd } from "./feature/board/BoardAdd.jsx";
 import { BoardList } from "./feature/board/BoardList.jsx";
 

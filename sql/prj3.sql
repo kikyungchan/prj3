274ae9b3f42a1bd2eba1c6016ba07b3c8f8c64b7
kikyungchan <kikyungchan@gmail.com>
2025-07-14 10:13:43 +0900

    게시물 테이블 변경

--- sql/prj3.sql
+++ sql/prj3.sql
@@ -4,10 +4,15 @@ CREATE TABLE board
     id          INT AUTO_INCREMENT NOT NULL,
     title       VARCHAR(99)        NOT NULL,
     content     VARCHAR(10000)     NOT NULL,
-    author      VARCHAR(100)       NOT NULL,
+    author      VARCHAR(255)       NOT NULL,
     inserted_at datetime           NOT NULL DEFAULT NOW(),
-    CONSTRAINT pk_board PRIMARY KEY (id)
+    CONSTRAINT pk_board PRIMARY KEY (id),
+    FOREIGN KEY (author) REFERENCES member (email)
 );
+
+
+DROP TABLE board;
+
 
 # 회원 테이블
 CREATE TABLE member
@@ -20,13 +25,17 @@ CREATE TABLE member
     CONSTRAINT pk_member PRIMARY KEY (email)
 );
 
+# 테스트용 정보 복사
 INSERT INTO board
     (title, content, author, inserted_at)
 SELECT title, content, author, inserted_at
 FROM board;
 
+# 정보 갯수 세기
 SELECT COUNT(*)
 FROM board;
+
+
 
 
 

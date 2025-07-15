70ddda56498a3b47f9cdfe3322ca4f889b00bb01
kikyungchan <kikyungchan@gmail.com>
2025-07-15 09:31:23 +0900

    댓글 저장 시 더블클릭으로 같은 댓글 등록 막기

--- frontend/src/feature/comment/CommentAdd.jsx
+++ frontend/src/feature/comment/CommentAdd.jsx
@@ -1,12 +1,14 @@
-import { Button, FormControl } from "react-bootstrap";
+import { Button, FormControl, Spinner } from "react-bootstrap";
 import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 
 export function CommentAdd({ boardId }) {
   const [comment, setComment] = useState("");
+  const [isProcessing, setIsProcessing] = useState(false);
 
   function handleCommentSaveClick() {
+    setIsProcessing(true);
     axios
       .post("/api/comment", { boardId: boardId, comment: comment })
       .then((res) => {
@@ -14,6 +16,7 @@ export function CommentAdd({ boardId }) {
         if (message) {
           toast(message.text, { type: message.type });
         }
+        setComment("");
       })
       .catch((err) => {
         const message = err.response.data.message;
@@ -21,7 +24,9 @@ export function CommentAdd({ boardId }) {
           toast(message.text, { type: message.type });
         }
       })
-      .finally(() => {});
+      .finally(() => {
+        setIsProcessing(false);
+      });
   }
 
   let saveButtonDisabled = false;
@@ -38,6 +43,7 @@ export function CommentAdd({ boardId }) {
         onChange={(e) => setComment(e.target.value)}
       />
       <Button disabled={saveButtonDisabled} onClick={handleCommentSaveClick}>
+        {isProcessing && <Spinner size="sm" />}
         댓글 저장
       </Button>
     </div>

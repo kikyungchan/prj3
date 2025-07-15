4aed54e2f3782fb703990d483c6020d968a21798
kikyungchan <kikyungchan@gmail.com>
2025-07-15 15:40:18 +0900

    댓글 수 표시 / 댓글 저장 버튼을 textarea 안쪽에

--- frontend/src/common/AppNavBar.jsx
+++ frontend/src/common/AppNavBar.jsx
@@ -89,7 +89,18 @@ export function AppNavBar() {
                   value={keyword}
                   onChange={(e) => setKeyword(e.target.value)}
                 ></FormControl>
-                <Button type="submit">검색</Button>
+                <Button type="submit">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-search"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                  </svg>
+                </Button>
               </InputGroup>
             </Form>
           </Navbar.Collapse>
--- frontend/src/feature/board/BoardDetail.jsx
+++ frontend/src/feature/board/BoardDetail.jsx
@@ -117,6 +117,9 @@ export function BoardDetail() {
             </Button>
           </div>
         )}
+        <div className="my-5">
+          <hr />
+        </div>
 
         {/*  댓글 컴포넌트 */}
         <CommentContainer boardId={board.id} />

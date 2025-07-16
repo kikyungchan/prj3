f386caa8d04fdc5e4e6ba45ae6e082aa40201e22
kikyungchan <kikyungchan@gmail.com>
2025-07-16 10:23:27 +0900

    좋아요 정보 얻기

---
+++ frontend/src/feature/like/LikeContainer.jsx
@@ -0,0 +1,46 @@
+import { GoHeart, GoHeartFill } from "react-icons/go";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
+
+export function LikeContainer({ boardId }) {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [likeInfo, setLikeInfo] = useState(null);
+
+  useEffect(() => {
+    if (!isProcessing) {
+      axios
+        .get(`/api/like/board/${boardId}`)
+        .then((res) => {
+          setLikeInfo(res.data);
+        })
+        .catch((err) => {})
+        .finally(() => {});
+    }
+  }, [isProcessing]);
+
+  function handleHeartClick() {
+    setIsProcessing(true);
+    axios
+      .put("/api/like", { boardId: boardId })
+      .then((res) => {})
+      .catch((err) => {})
+      .finally(() => {
+        setIsProcessing(false);
+      });
+  }
+
+  if (!likeInfo) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className="d-flex gap-2 h2">
+      <div onClick={handleHeartClick}>
+        {likeInfo.liked ? <GoHeartFill /> : <GoHeart />}
+      </div>
+
+      <div>{likeInfo.count}</div>
+    </div>
+  );
+}
